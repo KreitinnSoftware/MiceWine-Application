b10ddcd2dbf7dccdd42d68950efc93619c42e4ed
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2025-01-27 17:20:21 +0200

    Filemanager fade animation and improvements

--- app/src/main/java/com/micewine/emu/fragments/FileManagerFragment.kt
+++ app/src/main/java/com/micewine/emu/fragments/FileManagerFragment.kt
@@ -1,20 +1,27 @@
 package com.micewine.emu.fragments
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.micewine.emu.R
 import com.micewine.emu.activities.MainActivity.Companion.fileManagerCwd
 import com.micewine.emu.activities.MainActivity.Companion.fileManagerDefaultDir
 import com.micewine.emu.adapters.AdapterFiles
 import com.micewine.emu.databinding.FragmentFileManagerBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 
-class FileManagerFragment: Fragment() {
+class FileManagerFragment : Fragment() {
     private var binding: FragmentFileManagerBinding? = null
     private var rootView: View? = null
 
@@ -32,35 +39,71 @@ class FileManagerFragment: Fragment() {
         refreshFiles()
         registerForContextMenu(recyclerView!!)
 
+        fragmentInstance = this
+
         return rootView
     }
 
     companion object {
         private var recyclerView: RecyclerView? = null
         private val fileList: MutableList<AdapterFiles.FileList> = ArrayList()
+        private var _fragmentInstance: FileManagerFragment? = null
+        private const val ANIMATION_DURATION = 300L
+
+        var fragmentInstance
+            get() = _fragmentInstance
+            set(value) {
+                _fragmentInstance = value
+            }
 
         fun refreshFiles() {
-            recyclerView?.adapter?.notifyItemRangeRemoved(0, fileList.count())
+            recyclerView?.let { rv ->
+                if (rv.tag as? Boolean == true) return
+                rv.tag = true
+                rv.animate()
+                    .alpha(0f)
+                    .setDuration(ANIMATION_DURATION / 2)
+                    .setInterpolator(AccelerateDecelerateInterpolator())
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            fragmentInstance?.lifecycleScope?.launch {
+                                val newFileList = withContext(Dispatchers.IO) {
+                                    val filesList = mutableListOf<AdapterFiles.FileList>()
+                                    if (fileManagerCwd != fileManagerDefaultDir) {
+                                        filesList.add(AdapterFiles.FileList(File("..")))
+                                    }
+                                    File(fileManagerCwd).listFiles()
+                                        ?.sorted()
+                                        ?.filter { it.isDirectory }
+                                        ?.forEach { filesList.add(AdapterFiles.FileList(it)) }
+                                    File(fileManagerCwd).listFiles()
+                                        ?.sorted()
+                                        ?.filter { it.isFile }
+                                        ?.forEach { filesList.add(AdapterFiles.FileList(it)) }
+                                    filesList
+                                }
 
-            fileList.clear()
+                                withContext(Dispatchers.Main) {
+                                    rv.post {
+                                        fileList.clear()
+                                        fileList.addAll(newFileList)
 
-            if (fileManagerCwd != fileManagerDefaultDir) {
-                addToAdapter(File(".."))
+                                        rv.adapter?.notifyDataSetChanged()
+                                        rv.animate()
+                                            .alpha(1f)
+                                            .setDuration(ANIMATION_DURATION / 2)
+                                            .setInterpolator(AccelerateDecelerateInterpolator())
+                                            .setListener(object : AnimatorListenerAdapter() {
+                                                override fun onAnimationEnd(animation: Animator) {
+                                                    rv.tag = false
+                                                }
+                                            })
+                                    }
+                                }
+                            }
+                        }
+                    })
             }
-
-            File(fileManagerCwd).listFiles()?.sorted()?.forEach {
-                if (it.isDirectory) {
-                    addToAdapter(it)
-                }
-            }
-
-            File(fileManagerCwd).listFiles()?.sorted()?.forEach {
-                if (it.isFile) {
-                    addToAdapter(it)
-                }
-            }
-
-            recyclerView?.adapter?.notifyItemRangeInserted(0, fileList.count())
         }
 
         fun deleteFile(filePath: String) {
@@ -71,10 +114,6 @@ class FileManagerFragment: Fragment() {
 
                 recyclerView?.adapter?.notifyItemRemoved(index)
             }
-        }
-
-        private fun addToAdapter(file: File) {
-            fileList.add(AdapterFiles.FileList(file))
         }
     }
 }
